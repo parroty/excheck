61a1f5844d179213bd99d923b96ba48a4955242d
parroty <parroty00@gmail.com>
2014-08-06 17:15:29 +0900

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExCheck.Mixfile do
 
   def project do
     [ app: :excheck,
-      version: "0.1.4",
+      version: "0.1.5",
       elixir: "~> 0.14.0 or ~> 0.15.0",
       deps: deps,
       description: description,
