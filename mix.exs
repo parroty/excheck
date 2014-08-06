947a0b127b312901693a546df1ccacab5ce216d7
parroty <parroty00@gmail.com>
2014-08-06 17:12:23 +0900

    Add elixir v0.15.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExCheck.Mixfile do
   def project do
     [ app: :excheck,
       version: "0.1.4",
-      elixir: "~> 0.14.0",
+      elixir: "~> 0.14.0 or ~> 0.15.0",
       deps: deps,
       description: description,
       package: package,
