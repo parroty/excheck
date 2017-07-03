573ed6053af649a2b67ae391f7ccc7e7adf8a2f2
Adam Rutkowski <hq@mtod.org>
2017-07-03 12:45:27 +0200

    triq agent fixup

--- lib/excheck.ex
+++ lib/excheck.ex
@@ -30,6 +30,7 @@ defmodule ExCheck do
   def start(_app, _type) do
     import Supervisor.Spec, warn: false
     children = [
+      worker(ExCheck.TriqAgent, [])
     ]
     Supervisor.start_link(children, strategy: :one_for_one)
   end
