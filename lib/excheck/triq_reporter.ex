e754cf236ad8c685aafb6948d5b187cda648a03c
Adam Rutkowski <hq@mtod.org>
2017-07-03 12:42:19 +0200

    Add triq reporter module

---
+++ lib/excheck/triq_reporter.ex
@@ -0,0 +1,29 @@
+defmodule ExCheck.TriqReporter do
+
+  def report(:pass, _) do
+    IO.write colorize(:blue, "_")
+  end
+  def report(:fail, false) do
+    IO.write colorize(:red, "x")
+  end
+  def report(:fail, _value) do
+    IO.write colorize(:red, "x")
+  end
+  def report(:check_failed, [count, _error]) do
+    :ok = ExCheck.TriqAgent.update_tests_count(count)
+  end
+  def report(:success, count) do
+    :ok = ExCheck.TriqAgent.update_tests_count(count)
+  end
+  def report(:counterexample, _counter_example), do: nil
+  def report(:skip, _), do: nil
+  def report(:testing, [_module, _fun]), do: nil
+
+  # FIXME colors must be made optional
+  defp colorize(escape, string) do
+    [escape, string, :reset]
+    |> IO.ANSI.format_fragment(true)
+    |> IO.iodata_to_binary
+  end
+end
+
