61ed9c217a43b51dfe89d7c1f945a9e6718b0fc9
Adam Rutkowski <hq@mtod.org>
2017-07-04 10:59:04 +0200

    Suppres failure print outs
    These should fail exactly like ExUnit

--- lib/excheck/triq_reporter.ex
+++ lib/excheck/triq_reporter.ex
@@ -3,12 +3,6 @@ defmodule ExCheck.TriqReporter do
   def report(:pass, _) do
     IO.write colorize(:blue, "_")
   end
-  def report(:fail, false) do
-    IO.write colorize(:red, "x")
-  end
-  def report(:fail, _value) do
-    IO.write colorize(:red, "x")
-  end
   def report(:check_failed, [count, _error]) do
     :ok = ExCheck.TriqAgent.update_tests_count(count)
   end
@@ -18,6 +12,8 @@ defmodule ExCheck.TriqReporter do
   def report(:counterexample, _counter_example), do: nil
   def report(:skip, _), do: nil
   def report(:testing, [_module, _fun]), do: nil
+  def report(:fail, false), do: nil
+  def report(:fail, _value), do: nil
 
   # FIXME colors must be made optional
   defp colorize(escape, string) do
